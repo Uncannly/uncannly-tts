85813b4538c08a6a3e90d30be0b7bb5d6fa1eedb
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-12-25 13:42:28 -0800

    surface endpoint via spark and send buffer in res instead of save as file; did need compile plugin after all too; enable cors [#136481443]

--- src/main/java/PhonemesToSpeech.java
+++ src/main/java/PhonemesToSpeech.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.BufferedOutputStream;
 import java.io.InputStream;
 
 import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
@@ -8,45 +7,58 @@ import com.ivona.services.tts.model.CreateSpeechRequest;
 import com.ivona.services.tts.model.CreateSpeechResult;
 import com.ivona.services.tts.model.Input;
 import com.ivona.services.tts.model.Voice;
+import static spark.Spark.*;
 
 public class PhonemesToSpeech {
-    private static IvonaSpeechCloudClient speechCloud;
+  public static void main(String[] args) {
+  	options("/*", (req, res) -> {
+      String accessControlRequestHeaders = req.headers("Access-Control-Request-Headers");
+      if (accessControlRequestHeaders != null) {
+        res.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+      }
+      String accessControlRequestMethod = req.headers("Access-Control-Request-Method");
+      if (accessControlRequestMethod != null) {
+          res.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+      }
+      return "OK";
+    });
 
-    private static void init() {
-        speechCloud = new IvonaSpeechCloudClient(
-                new ClasspathPropertiesFileCredentialsProvider("resources/IvonaCredentials.properties"));
-        speechCloud.setEndpoint("https://tts.eu-west-1.ivonacloud.com");
-    }
+		before((req, res) -> res.header("Access-Control-Allow-Origin", "*"));
 
-    public static void main(String[] args) throws Exception {
-        init();
-        String outputFileName = "./speech.mp3";
-        CreateSpeechRequest createSpeechRequest = new CreateSpeechRequest();
-        Input input = new Input();
-        Voice voice = new Voice();
-        voice.setName("Salli");
-        input.setType("application/ssml+xml");
-        input.setData(String.format("<speak><phoneme alphabet=\"ipa\" ph=\"%s\">_</phoneme></speak>\"", args[0]));
-        createSpeechRequest.setInput(input);
-        createSpeechRequest.setVoice(voice);
-        InputStream in = null;
-        FileOutputStream outputStream = null;
-        try {
-            CreateSpeechResult createSpeechResult = speechCloud.createSpeech(createSpeechRequest);
-            in = createSpeechResult.getBody();
-            outputStream = new FileOutputStream(new File(outputFileName));
-            byte[] buffer = new byte[2 * 1024];
-            int readBytes;
-            while ((readBytes = in.read(buffer)) > 0) {
-                outputStream.write(buffer, 0, readBytes);
-            }
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-            if (outputStream != null) {
-                outputStream.close();
-            }
+    get("/pts", (req, res) -> {
+    	IvonaSpeechCloudClient speechCloud = new IvonaSpeechCloudClient(
+        new ClasspathPropertiesFileCredentialsProvider("resources/IvonaCredentials.properties"));
+      speechCloud.setEndpoint("https://tts.eu-west-1.ivonacloud.com");
+      String outputFileName = "./speech.mp3";
+      CreateSpeechRequest createSpeechRequest = new CreateSpeechRequest();
+      Input input = new Input();
+      Voice voice = new Voice();
+      voice.setName("Salli");
+      input.setType("application/ssml+xml");
+      input.setData(String.format("<speak><phoneme alphabet=\"ipa\" ph=\"%s\">_</phoneme></speak>\"", req.queryParams("word")));
+      createSpeechRequest.setInput(input);
+      createSpeechRequest.setVoice(voice);
+      InputStream in = null;
+      BufferedOutputStream outputStream = null;
+      res.raw().setContentType("application/octet-stream");
+      try {
+        CreateSpeechResult createSpeechResult = speechCloud.createSpeech(createSpeechRequest);
+        in = createSpeechResult.getBody();
+        outputStream = new BufferedOutputStream(res.raw().getOutputStream());
+        byte[] buffer = new byte[2 * 1024];
+        int readBytes;
+        while ((readBytes = in.read(buffer)) > 0) {
+          outputStream.write(buffer, 0, readBytes);
         }
-    }
+      } finally {
+        if (in != null) {
+          in.close();
+        }
+        if (outputStream != null) {
+          outputStream.close();
+        }
+      }
+      return res.raw();
+    });
+  }
 }
